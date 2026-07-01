b61cec8292506f04204ea7824fa1d43f36e75b45
delezg <david.elez@msmk.university>
2026-07-02 01:13:05 +0200

    Añadir Jenkinsfile

--- main.cpp
+++ main.cpp
@@ -2,13 +2,15 @@
 
 int main() {
 
-    int a=10;
-    int b=20;
-    int c=30;
-    int d=40;
-    int e=50;
+    int a = 10;
+    int b = 20;
+    int c = 30;
+    int d = 40;
+    int e = 50;
 
-    std::cout << a+b+c+d+e << std::endl;
+    int total = a + b + c + d + e;
+
+    std::cout << total << std::endl;
 
     return 0;
 }
